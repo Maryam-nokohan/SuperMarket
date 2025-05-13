4cf84469832aa38725204d75f3b6d8ca4e0bf31d
mary <maryam.nokohan@gmail.com>
2025-05-13 09:30:33 +0330

    added empty main.cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "include/Bank.hpp"
+#include "include/Currency.hpp"
+#include "include/USD.hpp"
+#include "include/EUR.hpp"
+#include "include/IRR.hpp"
+using namespace std;
+int main()
+{
+
+
+
+
+    return 0;
+}
