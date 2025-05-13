00b68aba9661fa9f395191e736d9afb0d95544e1
mary <maryam.nokohan@gmail.com>
2025-05-13 09:28:02 +0330

    add interface for bank.hpp

--- include/Bank.hpp
+++ include/Bank.hpp
@@ -1,9 +1,28 @@
 #ifndef BANK_H
 #define BANK_H
-class Bank
+#include "include/Currency.hpp"
+class BankAccount
 {
 private:
+std :: string AccountName;
+long long unsigned int AccountNumber;
+Currency * Balance;
+double DailyLimit;
 public:
+BankAccount(const std :: string & n , const long long unsigned int num , Currency * currency , double limit);
+virtual ~BankAccount();
+bool Deposit(Currency * a);
+bool WithDraw(Currency * a);
+Currency * GetBalance()const;
+std :: string GetAccountName()const;
+};
+class PersonalAccount : public BankAccount{
+    public :
+    PersonalAccount(const std :: string & n , const long long unsigned int num , Currency * currency );
+
+};
+class OrganizationAccount : public  BankAccount{
+OrganizationAccount(const std :: string & n , const long long unsigned int num , Currency * currency );
 };
 
 #endif
