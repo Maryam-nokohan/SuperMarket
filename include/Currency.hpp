0fdca2968d96dec51637396ee17ff89107946a6b
mary <maryam.nokohan@gmail.com>
2025-05-13 09:28:41 +0330

    added Currency interface

--- include/Currency.hpp
+++ include/Currency.hpp
@@ -1,11 +1,12 @@
 #ifndef CURRENCY_H
 #define CURRENCY_H
 #include <string>
+#include <include/Bank.hpp>
 class Currency
 {
     // friend std ::ostream &operator<<(std ::ostream &output, const Currency &);
     // friend std ::istream &operator>>(std ::istream &input, Currency &);
-
+    friend class BankAccount;
 protected:
     double amount;
 
@@ -15,10 +16,10 @@ public:
     double GetAmount() const;
     virtual double ToUSD() const = 0;
     virtual double FromUSD(double) = 0;
-    Currency &operator+(const Currency &);
-    Currency &operator-(const Currency &);
-    Currency &operator*(const Currency &);
-    Currency &operator/(const Currency &);
+    Currency &operator+=(const Currency &);
+    Currency &operator-=(const Currency &);
+    Currency &operator*=(const Currency &);
+    Currency &operator/=(const Currency &);
     bool operator==(const Currency &);
     bool operator!=(const Currency &);
     bool operator>(const Currency &);
