0a243e4fb9ade191792a307d3354cb489de41b3e
saba <sababahrami202@gmail.com>
2025-05-15 15:31:25 +0330

    writing Seasoning class

--- include/Seasoning.hpp
+++ include/Seasoning.hpp
@@ -1,3 +1,12 @@
-#ifndef SEASONING_H
-#define SEASONING_H
+#ifndef SEASONING_HPP
+#define SEASONING_HPP
+#include "Item.hpp"
+class Seasoning : public Item{
+    public:
+    Seasoning(const std::string& , double , double);
+    std::string getUnit() const override;
+    void print(std::ostream&) const override;
+    Item* copy() const override;
+
+};
 #endif
