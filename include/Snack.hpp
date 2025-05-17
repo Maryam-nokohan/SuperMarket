54ccc1c091e517a3087a25845070d110a03c083b
saba <sababahrami202@gmail.com>
2025-05-17 14:47:19 +0330

    added snack

--- include/Snack.hpp
+++ include/Snack.hpp
@@ -1,3 +1,12 @@
-#ifndef SNACK_H
-#define SNACK_H
+#ifndef SNACK_HPP
+#define SNACK_HPP
+#include "Item.hpp"
+class Snack : public Item{
+    public:
+    Snack(const std::string&, double , double);
+    std::string getUnit() const override ;
+    void print(std::ostream&) const override;
+    Item* copy() const override ;
+
+};
 #endif
